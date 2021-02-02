64ac464b7fa7af91c1440db628d6103c9b3e3ea4
Ken Smith <k.w.smith@qub.ac.uk>
2021-02-02 15:27:25 +0000

    Added install-mysql-sherlock role.

---
+++ gkansible/roles/install-mysql-sherlock/tasks/sherlock_innodb_discard_tablespaces.sql
@@ -0,0 +1,4 @@
+alter table tcs_cat_glade_v2_3 discard tablespace;
+alter table tcs_cat_iras_revised_bright_galaxy_sample_final discard tablespace;
+alter table tcs_cat_ppmxl_final discard tablespace;
+alter table tcs_helper_trickle_streams discard tablespace;
---
+++ gkansible/roles/install-mysql-sherlock/tasks/sherlock_innodb_import_tablespaces.sql
@@ -0,0 +1,4 @@
+alter table tcs_cat_glade_v2_3 import tablespace;
+alter table tcs_cat_iras_revised_bright_galaxy_sample_final import tablespace;
+alter table tcs_cat_ppmxl_final import tablespace;
+alter table tcs_helper_trickle_streams import tablespace;
